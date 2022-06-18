f004ad3ccf234d9823e423a9900bf3c41e3b29e2
Denis Buserski <denis.buserski@gmail.com>
2022-06-18 23:06:44 +0300

    Update Season.java

--- Working_With_Abstraction_Lab/Hotel_Reservation_04/Season.java
+++ Working_With_Abstraction_Lab/Hotel_Reservation_04/Season.java
@@ -1,11 +1,10 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Working_With_Abstraction_Lab.Hotel_Reservation_04;
+package Working_With_Abstraction_Lab.Hotel_Reservation_04;
 
 public enum Season {
     AUTUMN(1),
     SPRING(2),
     WINTER(3),
     SUMMER(4);
-
 
     private final int multiplier;
 
@@ -16,4 +15,5 @@ public enum Season {
     public double getMultiplier() {
         return multiplier;
     }
+    
 }
