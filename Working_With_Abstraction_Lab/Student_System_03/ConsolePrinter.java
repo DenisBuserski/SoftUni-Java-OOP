2bdd99a5c8d8ef0284565445c6d6495dd87981a4
Denis Buserski <denis.buserski@gmail.com>
2022-06-19 19:04:18 +0300

    Update ConsolePrinter.java

--- Working_With_Abstraction_Lab/Student_System_03/ConsolePrinter.java
+++ Working_With_Abstraction_Lab/Student_System_03/ConsolePrinter.java
@@ -1,7 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Working_With_Abstraction_Lab.Student_System_03;
+package Working_With_Abstraction_Lab.Student_System_03;
 
 public class ConsolePrinter {
-
     public static void printLine(String str) {
         System.out.println(str);
     }
