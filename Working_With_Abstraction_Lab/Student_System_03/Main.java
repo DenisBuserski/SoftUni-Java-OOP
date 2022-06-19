8aa12e647ba5db8421050d03597ee3f9c6fbfa30
Denis Buserski <denis.buserski@gmail.com>
2022-06-19 19:05:02 +0300

    Update Main.java

--- Working_With_Abstraction_Lab/Student_System_03/Main.java
+++ Working_With_Abstraction_Lab/Student_System_03/Main.java
@@ -1,11 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Working_With_Abstraction_Lab.Student_System_03;
+package Working_With_Abstraction_Lab.Student_System_03;
 
 public class Main {
     public static void main(String[] args) {
-
         StudentSystem system = new StudentSystem();
 
         system.start();
-
     }
 }
