9845f4ece1be98f7736434a9cefb3d8f30ecfbb1
Denis Buserski <denis.buserski@gmail.com>
2022-06-19 19:20:16 +0300

    Update Student.java

--- Working_With_Abstraction_Lab/Student_System_03/Student.java
+++ Working_With_Abstraction_Lab/Student_System_03/Student.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Working_With_Abstraction_Lab.Student_System_03;
+package Working_With_Abstraction_Lab.Student_System_03;
 
 public class Student {
     private String name;
