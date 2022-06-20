e7a8fb15e9486c3a9145a2f3bf4af354f6e804dd
Denis Buserski <denis.buserski@gmail.com>
2022-06-20 21:47:10 +0300

    Add files via upload

---
+++ Encapsulation_Lab/Sort_By_Name_And_Age_01/Person.java
@@ -0,0 +1,27 @@
+package Encapsulation_Lab.Sort_By_Name_And_Age_01;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.", firstName, lastName, age);
+    }
+
+}
