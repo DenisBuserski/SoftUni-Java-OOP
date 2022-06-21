62c8e3b4c7b7b031fc27d514569379ba26960ad9
Denis Buserski <denis.buserski@gmail.com>
2022-06-21 22:36:48 +0300

    Update Main.java

--- Encapsulation_Exercises/Pizza_Calories_04/Main.java
+++ Encapsulation_Exercises/Pizza_Calories_04/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Encapsulation_Exercises.Pizza_Calories_04;
+package Encapsulation_Exercises.Pizza_Calories_04;
 
 public class Main {
     public static void main(String[] args) {
