a4e71432a9f518ff48f0a3d2937e67fc2b8c1530
Denis Buserski <denis.buserski@gmail.com>
2022-06-21 22:33:46 +0300

    Update Main.java

--- Encapsulation_Exercises/Shopping_Spree_03/Main.java
+++ Encapsulation_Exercises/Shopping_Spree_03/Main.java
@@ -1,11 +1,10 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Encapsulation_Exercises.Shopping_Spree_03;
+package Encapsulation_Exercises.Shopping_Spree_03;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
 
     }
 }
