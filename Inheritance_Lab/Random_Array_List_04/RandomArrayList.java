e1bb38d697e73148d96f5378b4a1b6854915403a
Denis Buserski <denis.buserski@gmail.com>
2022-06-22 23:02:57 +0300

    Update RandomArrayList.java

--- Inheritance_Lab/Random_Array_List_04/RandomArrayList.java
+++ Inheritance_Lab/Random_Array_List_04/RandomArrayList.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Lab.Random_Array_List_04;
+package Inheritance_Lab.Random_Array_List_04;
 
 import java.util.ArrayList;
 import java.util.Random;
