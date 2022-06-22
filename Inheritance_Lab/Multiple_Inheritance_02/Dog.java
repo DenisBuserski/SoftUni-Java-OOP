8bd60b8dc95b311696078f6f0c898ce05335f4b7
Denis Buserski <denis.buserski@gmail.com>
2022-06-22 22:59:06 +0300

    Update Dog.java

--- Inheritance_Lab/Multiple_Inheritance_02/Dog.java
+++ Inheritance_Lab/Multiple_Inheritance_02/Dog.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Lab.Multiple_Inheritance_02;
+package Inheritance_Lab.Multiple_Inheritance_02;
 
 public class Dog extends Animal {
 
