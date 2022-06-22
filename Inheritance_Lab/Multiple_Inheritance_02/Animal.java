3457349f81de3de2e5cffc470b77703d7d5cb9a4
Denis Buserski <denis.buserski@gmail.com>
2022-06-22 22:58:20 +0300

    Update Animal.java

--- Inheritance_Lab/Multiple_Inheritance_02/Animal.java
+++ Inheritance_Lab/Multiple_Inheritance_02/Animal.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Lab.Multiple_Inheritance_02;
+package Inheritance_Lab.Multiple_Inheritance_02;
 
 public class Animal {
 
     public void eat() {
         System.out.println("eating…");
     }
-
 }
