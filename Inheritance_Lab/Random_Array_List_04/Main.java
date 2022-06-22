5af4e56157a9e4c994e95b9257ae8cf7d0aaedbb
Denis Buserski <denis.buserski@gmail.com>
2022-06-22 23:02:17 +0300

    Update Main.java

--- Inheritance_Lab/Random_Array_List_04/Main.java
+++ Inheritance_Lab/Random_Array_List_04/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Lab.Random_Array_List_04;
+package Inheritance_Lab.Random_Array_List_04;
 
 public class Main {
     public static void main(String[] args) {
