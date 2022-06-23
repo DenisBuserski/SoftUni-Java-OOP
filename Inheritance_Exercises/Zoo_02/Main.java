77b7954d17e5c0603ac5daa27c2806cc518daa94
Denis Buserski <denis.buserski@gmail.com>
2022-06-23 23:16:57 +0300

    Update Main.java

--- Inheritance_Exercises/Zoo_02/Main.java
+++ Inheritance_Exercises/Zoo_02/Main.java
@@ -1,4 +1,5 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Zoo_02;
+package Inheritance_Exercises.Zoo_02;
 
 public class Main {
+  
 }
