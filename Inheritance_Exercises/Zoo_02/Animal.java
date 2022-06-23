3a433cd05543b9fd8505b8e843b8b09ddf327132
Denis Buserski <denis.buserski@gmail.com>
2022-06-23 23:15:05 +0300

    Update Animal.java

--- Inheritance_Exercises/Zoo_02/Animal.java
+++ Inheritance_Exercises/Zoo_02/Animal.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Zoo_02;
+package Inheritance_Exercises.Zoo_02;
 
 public class Animal {
     private String name;
@@ -10,4 +10,5 @@ public class Animal {
     public String getName() {
         return name;
     }
+    
 }
