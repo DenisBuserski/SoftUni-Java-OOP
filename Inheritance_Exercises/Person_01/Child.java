b6774206b7bc27baf3bb407e2f7d76d603d1d4cb
Denis Buserski <denis.buserski@gmail.com>
2022-06-23 23:13:57 +0300

    Update Child.java

--- Inheritance_Exercises/Person_01/Child.java
+++ Inheritance_Exercises/Person_01/Child.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Person_01;
+package Inheritance_Exercises.Person_01;
 
 public class Child extends Person{
 
