f1cef929cc750a74e9e58cb96aa71c4253a1c61d
Denis Buserski <denis.buserski@gmail.com>
2022-06-23 23:18:30 +0300

    Update Snake.java

--- Inheritance_Exercises/Zoo_02/Snake.java
+++ Inheritance_Exercises/Zoo_02/Snake.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Zoo_02;
+package Inheritance_Exercises.Zoo_02;
 
 public class Snake extends Reptile {
 
