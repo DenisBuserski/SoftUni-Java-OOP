a92676f504fca17ea6da9556670e122e50407d20
Denis Buserski <denis.buserski@gmail.com>
2022-06-25 15:04:35 +0300

    Update CrossMotorcycle.java

--- Inheritance_Exercises/Need_For_Speed_04/CrossMotorcycle.java
+++ Inheritance_Exercises/Need_For_Speed_04/CrossMotorcycle.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Need_For_Speed_04;
+package Inheritance_Exercises.Need_For_Speed_04;
 
 public class CrossMotorcycle extends Motorcycle {
 
