a5ec131af327dd634c33f6ccf74f457f2a07485d
Denis Buserski <denis.buserski@gmail.com>
2022-06-25 15:06:03 +0300

    Update Motorcycle.java

--- Inheritance_Exercises/Need_For_Speed_04/Motorcycle.java
+++ Inheritance_Exercises/Need_For_Speed_04/Motorcycle.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Need_For_Speed_04;
+package Inheritance_Exercises.Need_For_Speed_04;
 
 public class Motorcycle extends Vehicle {
 
     public Motorcycle(double fuel, int horsePower) {
         super(fuel, horsePower);
     }
-
 }
