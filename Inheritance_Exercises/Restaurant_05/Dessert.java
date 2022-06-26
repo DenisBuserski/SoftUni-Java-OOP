e1552a2ed565676a4cbd9e638a3e6f206cc89ffd
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:15:53 +0300

    Update Dessert.java

--- Inheritance_Exercises/Restaurant_05/Dessert.java
+++ Inheritance_Exercises/Restaurant_05/Dessert.java
@@ -1,17 +1,17 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 import java.math.BigDecimal;
 
 public class Dessert extends Food {
-
     private double calories;
 
     public Dessert(String name, BigDecimal price, double grams, double calories) {
         super(name, price, grams);
         this.calories = calories;
     }
-
-   public double getCalories() {
+    
+    public double getCalories() {
         return calories;
-   }
+    }
+    
 }
