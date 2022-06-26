4d3de67bcb8d34f093c22c0747cf1d1c58df20ef
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:20:20 +0300

    Update Main.java

--- Inheritance_Exercises/Restaurant_05/Main.java
+++ Inheritance_Exercises/Restaurant_05/Main.java
@@ -1,4 +1,5 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 public class Main {
+  
 }
