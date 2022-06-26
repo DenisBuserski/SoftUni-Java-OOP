73b660f59a4b77a4e992ba9d01837ac389f32bbe
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:21:04 +0300

    Update MainDish.java

--- Inheritance_Exercises/Restaurant_05/MainDish.java
+++ Inheritance_Exercises/Restaurant_05/MainDish.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 import java.math.BigDecimal;
 
