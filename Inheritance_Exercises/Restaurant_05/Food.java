42391fb0a32c19974ee64dde2c1dc5ee3b96b44c
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:18:37 +0300

    Update Food.java

--- Inheritance_Exercises/Restaurant_05/Food.java
+++ Inheritance_Exercises/Restaurant_05/Food.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 import java.math.BigDecimal;
 
 public class Food extends Product {
-
     private double grams;
 
     public Food(String name, BigDecimal price, double grams) {
@@ -14,4 +13,5 @@ public class Food extends Product {
     public double getGrams() {
         return grams;
     }
+    
 }
