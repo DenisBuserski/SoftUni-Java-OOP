c06655431cd06f0c0f6153f7937860fdbb867138
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:11:23 +0300

    Update Coffee.java

--- Inheritance_Exercises/Restaurant_05/Coffee.java
+++ Inheritance_Exercises/Restaurant_05/Coffee.java
@@ -1,20 +1,19 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 import java.math.BigDecimal;
 
 public class Coffee extends HotBeverage {
-
     private static final double COFFEE_MILLILITERS = 50;
     private static final BigDecimal COFFEE_PRICE = new BigDecimal(3.50);
     private double caffeine;
 
-
     public Coffee(String name, double caffeine) {
-        super(name, COFFEE_PRICE , COFFEE_MILLILITERS);
+        super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
         this.caffeine = caffeine;
     }
 
     public double getCaffeine() {
         return caffeine;
     }
+    
 }
