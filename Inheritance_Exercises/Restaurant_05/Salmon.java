2180ca604f82c7776e73a9672fa2dfc48b08a5d9
Denis Buserski <denis.buserski@gmail.com>
2022-06-26 12:24:52 +0300

    Update Salmon.java

--- Inheritance_Exercises/Restaurant_05/Salmon.java
+++ Inheritance_Exercises/Restaurant_05/Salmon.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Restaurant_05;
+package Inheritance_Exercises.Restaurant_05;
 
 import java.math.BigDecimal;
 
 public class Salmon extends MainDish {
-
     private static final double SALMON_GRAMS = 22;
 
     public Salmon(String name, BigDecimal price) {
