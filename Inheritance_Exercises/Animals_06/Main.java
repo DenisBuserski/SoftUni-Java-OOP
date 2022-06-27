6186a68f93e607a2408262cbd3920627adcdaf53
Denis Buserski <denis.buserski@gmail.com>
2022-06-27 22:25:03 +0300

    Update Main.java

--- Inheritance_Exercises/Animals_06/Main.java
+++ Inheritance_Exercises/Animals_06/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Inheritance_Exercises.Animals_06;
+package Inheritance_Exercises.Animals_06;
 
 public class Main {
 
