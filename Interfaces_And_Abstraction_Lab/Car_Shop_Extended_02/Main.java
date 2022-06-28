3ec1c3378f2c766d518a5c5657a8ddf6b0e1cfd3
Denis Buserski <denis.buserski@gmail.com>
2022-06-28 06:32:45 +0300

    Update Main.java

--- Interfaces_And_Abstraction_Lab/Car_Shop_Extended_02/Main.java
+++ Interfaces_And_Abstraction_Lab/Car_Shop_Extended_02/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Car_Shop_Extended_02;
+package Interfaces_And_Abstraction_Lab.Car_Shop_Extended_02;
 
 public class Main {
     public static void main(String[] args) {
