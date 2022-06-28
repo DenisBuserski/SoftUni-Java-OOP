836238c3624f880ea633dc237d2f5e7e75d1ecb4
Denis Buserski <denis.buserski@gmail.com>
2022-06-28 06:33:40 +0300

    Update Rentable.java

--- Interfaces_And_Abstraction_Lab/Car_Shop_Extended_02/Rentable.java
+++ Interfaces_And_Abstraction_Lab/Car_Shop_Extended_02/Rentable.java
@@ -1,7 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Car_Shop_Extended_02;
+package Interfaces_And_Abstraction_Lab.Car_Shop_Extended_02;
 
 public interface Rentable {
 
     Integer getMinRentDay();
+    
     Double getPricePerDay();
+    
 }
