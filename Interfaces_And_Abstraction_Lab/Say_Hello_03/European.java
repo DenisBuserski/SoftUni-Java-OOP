51f18d1bc8c2b968a25b1b7c9bd7f1490ad2de44
Denis Buserski <denis.buserski@gmail.com>
2022-06-28 06:41:12 +0300

    Update European.java

--- Interfaces_And_Abstraction_Lab/Say_Hello_03/European.java
+++ Interfaces_And_Abstraction_Lab/Say_Hello_03/European.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Say_Hello_03;
+package Interfaces_And_Abstraction_Lab.Say_Hello_03;
 
 public class European implements Person {
     private String name;
@@ -16,4 +16,5 @@ public class European implements Person {
     public String sayHello() {
         return "Hello";
     }
+    
 }
