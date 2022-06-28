d4e8248c37108eae298ab4ebf099d2ee23e40191
Denis Buserski <denis.buserski@gmail.com>
2022-06-28 06:25:10 +0300

    Update Car.java

--- Interfaces_And_Abstraction_Lab/Car_Shop_01/Car.java
+++ Interfaces_And_Abstraction_Lab/Car_Shop_01/Car.java
@@ -1,9 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Car_Shop_01;
+package Interfaces_And_Abstraction_Lab.Car_Shop_01;
 
 import java.io.Serializable;
 
 public interface Car extends Serializable {
-
     int TIRES = 4;
 
     String getModel();
