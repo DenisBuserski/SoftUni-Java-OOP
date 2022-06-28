6088507397c70224c467d02d379d201b91834209
Denis Buserski <denis.buserski@gmail.com>
2022-06-28 06:42:29 +0300

    Update Main.java

--- Interfaces_And_Abstraction_Lab/Say_Hello_03/Main.java
+++ Interfaces_And_Abstraction_Lab/Say_Hello_03/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Say_Hello_03;
+package Interfaces_And_Abstraction_Lab.Say_Hello_03;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,7 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
-
 
         persons.add(new Bulgarian("Peter"));
         persons.add(new European("Peter"));
@@ -20,4 +19,5 @@ public class Main {
     private static void print(Person person) {
         System.out.println(person.sayHello());
     }
+    
 }
