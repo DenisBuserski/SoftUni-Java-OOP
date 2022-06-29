d7b46dee7e6f13201498beaf726725b23556908e
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:26:29 +0300

    Update Main.java

--- Interfaces_And_Abstraction_Lab/Ferrari_06/Main.java
+++ Interfaces_And_Abstraction_Lab/Ferrari_06/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Ferrari_06;
+package Interfaces_And_Abstraction_Lab.Ferrari_06;
 
 import java.util.Scanner;
 
