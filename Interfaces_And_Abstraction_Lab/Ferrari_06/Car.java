d76552b0302e7caef2b1f44aa71c4966fd9e439a
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:24:19 +0300

    Update Car.java

--- Interfaces_And_Abstraction_Lab/Ferrari_06/Car.java
+++ Interfaces_And_Abstraction_Lab/Ferrari_06/Car.java
@@ -1,6 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Ferrari_06;
+package Interfaces_And_Abstraction_Lab.Ferrari_06;
 
 public interface Car {
+    
     String brakes();
+    
     String gas();
+    
 }
