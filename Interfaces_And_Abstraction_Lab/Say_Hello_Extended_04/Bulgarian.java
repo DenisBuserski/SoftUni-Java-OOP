609d6f6f86beb50dc8989253b57e19a93448fd6d
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:16:02 +0300

    Update Bulgarian.java

--- Interfaces_And_Abstraction_Lab/Say_Hello_Extended_04/Bulgarian.java
+++ Interfaces_And_Abstraction_Lab/Say_Hello_Extended_04/Bulgarian.java
@@ -1,6 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Say_Hello_Extended_04;
+package Interfaces_And_Abstraction_Lab.Say_Hello_Extended_04;
 
-public class Bulgarian extends BasePerson implements Person{
+public class Bulgarian extends BasePerson implements Person {
 
     public Bulgarian(String name) {
         super(name);
@@ -10,4 +10,5 @@ public class Bulgarian extends BasePerson implements Person{
     public String sayHello() {
         return "Здравей";
     }
+    
 }
