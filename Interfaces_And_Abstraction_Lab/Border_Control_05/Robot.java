03a030009ae13227a0548ad8ecb8b821551dcd34
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:23:08 +0300

    Update Robot.java

--- Interfaces_And_Abstraction_Lab/Border_Control_05/Robot.java
+++ Interfaces_And_Abstraction_Lab/Border_Control_05/Robot.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Border_Control_05;
+package Interfaces_And_Abstraction_Lab.Border_Control_05;
 
 public class Robot implements Identifiable {
     private String id;
@@ -17,4 +17,5 @@ public class Robot implements Identifiable {
     public String getId() {
         return this.id;
     }
+    
 }
