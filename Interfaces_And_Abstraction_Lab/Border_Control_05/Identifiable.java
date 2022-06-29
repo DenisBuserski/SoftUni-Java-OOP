e14c81a897fb552d4c0f00531c78308c22ac538b
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:21:10 +0300

    Update Identifiable.java

--- Interfaces_And_Abstraction_Lab/Border_Control_05/Identifiable.java
+++ Interfaces_And_Abstraction_Lab/Border_Control_05/Identifiable.java
@@ -1,5 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Border_Control_05;
+package Interfaces_And_Abstraction_Lab.Border_Control_05;
 
 public interface Identifiable {
+    
     String getId();
+    
 }
