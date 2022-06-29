8565a105416ff6da43e490538bdc2aec60c2bfe1
Denis Buserski <denis.buserski@gmail.com>
2022-06-29 06:19:10 +0300

    Update Person.java

--- Interfaces_And_Abstraction_Lab/Say_Hello_Extended_04/Person.java
+++ Interfaces_And_Abstraction_Lab/Say_Hello_Extended_04/Person.java
@@ -1,6 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Lab.Say_Hello_Extended_04;
+package Interfaces_And_Abstraction_Lab.Say_Hello_Extended_04;
 
 public interface Person {
+    
     String getName();
+    
     String sayHello();
+        
 }
