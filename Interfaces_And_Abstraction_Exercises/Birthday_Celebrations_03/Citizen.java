d16489323528670b315e6ccb54cfd6dc8c57c57c
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:37:39 +0300

    Update Citizen.java

--- Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Citizen.java
+++ Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Citizen.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
+package Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
 
 public class Citizen implements Person, Identifiable, Birthable {
     private String name;
@@ -32,4 +32,5 @@ public class Citizen implements Person, Identifiable, Birthable {
     public String getId() {
         return this.id;
     }
+    
 }
