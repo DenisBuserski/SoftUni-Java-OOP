6cdfbf5e0d9825c39dbfc067d62b10f11ff95813
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:37:03 +0300

    Update Birthable.java

--- Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Birthable.java
+++ Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Birthable.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
+package Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
 
 public interface Birthable {
     String getBirthDate();
