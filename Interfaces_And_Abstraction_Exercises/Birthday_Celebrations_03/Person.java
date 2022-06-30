5d7c40e4fc16dafc08c1f6d1dc65e822d00accbe
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:39:18 +0300

    Update Person.java

--- Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Person.java
+++ Interfaces_And_Abstraction_Exercises/Birthday_Celebrations_03/Person.java
@@ -1,6 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
+package Interfaces_And_Abstraction_Exercises.Birthday_Celebrations_03;
 
 public interface Person {
     String getName();
+    
     int getAge();
 }
