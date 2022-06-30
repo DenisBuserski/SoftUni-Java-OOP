95df9100ee24b08af32289586e983dd5e8253c6a
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:33:46 +0300

    Update Person.java

--- Interfaces_And_Abstraction_Exercises/Define_An_Interface_Person_01/Person.java
+++ Interfaces_And_Abstraction_Exercises/Define_An_Interface_Person_01/Person.java
@@ -1,6 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Define_An_Interface_Person_01;
+package Interfaces_And_Abstraction_Exercises.Define_An_Interface_Person_01;
 
 public interface Person {
     String getName();
+   
     int getAge();
 }
