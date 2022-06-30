346149bcdc5e18359e59e4cf82fa1f7e2419ea38
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:34:56 +0300

    Update Citizen.java

--- Interfaces_And_Abstraction_Exercises/Multiple_Implementation_02/Citizen.java
+++ Interfaces_And_Abstraction_Exercises/Multiple_Implementation_02/Citizen.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Multiple_Implementation_02;
+package Interfaces_And_Abstraction_Exercises.Multiple_Implementation_02;
 
 public class Citizen implements Person, Identifiable, Birthable {
     private String name;
@@ -32,4 +32,5 @@ public class Citizen implements Person, Identifiable, Birthable {
     public String getId() {
         return this.id;
     }
+    
 }
