224ae1effb7e1047055800fc2153e495d6a1f75a
Denis Buserski <denis.buserski@gmail.com>
2022-06-30 23:32:20 +0300

    Update Citizen.java

--- Interfaces_And_Abstraction_Exercises/Define_An_Interface_Person_01/Citizen.java
+++ Interfaces_And_Abstraction_Exercises/Define_An_Interface_Person_01/Citizen.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Define_An_Interface_Person_01;
+package Interfaces_And_Abstraction_Exercises.Define_An_Interface_Person_01;
 
 public class Citizen implements Person {
     private String name;
@@ -18,4 +18,5 @@ public class Citizen implements Person {
     public int getAge() {
         return this.age;
     }
+    
 }
