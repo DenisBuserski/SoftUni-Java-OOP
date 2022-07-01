22f5744e90fbc9deeb0dfe614e89b8ae3d121cc8
Denis Buserski <denis.buserski@gmail.com>
2022-07-01 08:06:31 +0300

    Update Buyer.java

--- Interfaces_And_Abstraction_Exercises/Food_Shortage_04/Buyer.java
+++ Interfaces_And_Abstraction_Exercises/Food_Shortage_04/Buyer.java
@@ -1,7 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Food_Shortage_04;
+package Interfaces_And_Abstraction_Exercises.Food_Shortage_04;
 
 public interface Buyer extends Person {
-
     void buyFood();
 
     int getFood();
