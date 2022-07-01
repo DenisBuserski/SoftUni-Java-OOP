738aee9b81ad068fc975509c73132251cc81e8a0
Denis Buserski <denis.buserski@gmail.com>
2022-07-01 08:16:22 +0300

    Update Browsable.java

--- Interfaces_And_Abstraction_Exercises/Telephony_05/Browsable.java
+++ Interfaces_And_Abstraction_Exercises/Telephony_05/Browsable.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Telephony_05;
+package Interfaces_And_Abstraction_Exercises.Telephony_05;
 
 public interface Browsable {
     String browse();
