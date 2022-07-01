47c54e68db2c0c8d991a7d95e4b9169d23863c04
Denis Buserski <denis.buserski@gmail.com>
2022-07-01 08:25:39 +0300

    Update MyList.java

--- Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Interfaces/MyList.java
+++ Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Interfaces/MyList.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces;
+package Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces;
 
 public interface MyList extends AddRemovable {
     int getUsed();
