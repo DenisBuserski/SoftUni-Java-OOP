ee0cb3fb2fc8b73b09176cefd7922ac5f83bd047
Denis Buserski <denis.buserski@gmail.com>
2022-07-02 07:33:51 +0300

    Update Main.java

--- Polymorphism_Lab/Math_Operation_01/Main.java
+++ Polymorphism_Lab/Math_Operation_01/Main.java
@@ -1,12 +1,11 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Lab.Math_Operation_01;
-
+package Polymorphism_Lab.Math_Operation_01;
 
 public class Main {
     public static void main(String[] args) {
         MathOperation math = new MathOperation();
+        
         System.out.println(math.add(2, 2));
         System.out.println(math.add(3, 3, 3));
         System.out.println(math.add(4, 4, 4, 4));
     }
-
 }
