f3dc8ed1220d3245791b88516d934f79b9002597
Denis Buserski <denis.buserski@gmail.com>
2022-07-02 07:11:49 +0300

    Update AddCollection.java

--- Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Entities/AddCollection.java
+++ Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Entities/AddCollection.java
@@ -1,10 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Entities;
+package Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Entities;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces.Addable;
-
+import Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces.Addable;
 
 public class AddCollection extends Collection implements Addable {
-
     private int index = 0;
 
     public AddCollection() {
