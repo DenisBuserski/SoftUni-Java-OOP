f3c0beecb976e9e2da5fe3560f79cf821b1cf13f
Denis Buserski <denis.buserski@gmail.com>
2022-07-02 07:46:31 +0300

    Update Dog.java

--- Polymorphism_Lab/Animals_03/Dog.java
+++ Polymorphism_Lab/Animals_03/Dog.java
@@ -1,6 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Lab.Animals_03;
+package Polymorphism_Lab.Animals_03;
 
 public class Dog extends Animal {
+    
     public Dog(String name, String favoriteFood) {
         super(name,favoriteFood);
     }
@@ -9,4 +10,5 @@ public class Dog extends Animal {
     public String explainSelf() {
         return String.format("%s%nDJAAF",super.explainSelf());
     }
+    
 }
