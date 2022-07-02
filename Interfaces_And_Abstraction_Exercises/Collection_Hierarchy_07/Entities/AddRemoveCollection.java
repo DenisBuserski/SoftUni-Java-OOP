00e12b30e8827c99c01dc49dcd18712f3faec1a2
Denis Buserski <denis.buserski@gmail.com>
2022-07-02 07:13:00 +0300

    Update AddRemoveCollection.java

--- Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Entities/AddRemoveCollection.java
+++ Interfaces_And_Abstraction_Exercises/Collection_Hierarchy_07/Entities/AddRemoveCollection.java
@@ -1,16 +1,16 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Entities;
+package Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Entities;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces.AddRemovable;
+import Interfaces_And_Abstraction_Exercises.Collection_Hierarchy_07.Interfaces.AddRemovable;
+
 public class AddRemoveCollection extends Collection implements AddRemovable {
 
     public AddRemoveCollection(){
         super();
     }
 
-
     @Override
     public String remove() {
-        return super.getItems().remove(super.getItems().size()-1);
+        return super.getItems().remove(super.getItems().size() - 1);
     }
 
     @Override
@@ -18,4 +18,5 @@ public class AddRemoveCollection extends Collection implements AddRemovable {
         super.getItems().add(0,str);
         return 0;
     }
+    
 }
