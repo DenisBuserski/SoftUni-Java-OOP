891fbdbc7065023a97d2947642646f7b1acecacd
Denis Buserski <denis.buserski@gmail.com>
2022-07-02 07:45:47 +0300

    Update Main.java

--- Polymorphism_Lab/Animals_03/Main.java
+++ Polymorphism_Lab/Animals_03/Main.java
@@ -1,11 +1,11 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Lab.Animals_03;
+package Polymorphism_Lab.Animals_03;
 
 public class Main {
     public static void main(String[] args) {
         Animal cat = new Cat("Oscar", "Whiskas");
         Animal dog = new Dog("Rocky", "Meat");
+        
         System.out.println(cat.explainSelf());
         System.out.println(dog.explainSelf());
     }
-
 }
