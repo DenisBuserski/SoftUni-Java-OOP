f3ec2937b9a25dfbb72b4ff35aef7905abadfd2e
Denis Buserski <denis.buserski@gmail.com>
2022-07-03 23:34:02 +0300

    Update Car.java

--- Polymorphism_Exercises/Vehicles_Extension_02/Car.java
+++ Polymorphism_Exercises/Vehicles_Extension_02/Car.java
@@ -1,11 +1,10 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Vehicles_Extension_02;
+package Polymorphism_Exercises.Vehicles_Extension_02;
 
 public class Car extends Vehicle {
-
     private static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 0.9;
 
     public Car(double fuel, double consumption, double tankCapacity) {
-
         super(fuel, consumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION, tankCapacity);
     }
+    
 }
