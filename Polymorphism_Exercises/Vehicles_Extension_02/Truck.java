c62fef183f0108039946457966ec7112a5e8eaae
Denis Buserski <denis.buserski@gmail.com>
2022-07-03 23:36:40 +0300

    Update Truck.java

--- Polymorphism_Exercises/Vehicles_Extension_02/Truck.java
+++ Polymorphism_Exercises/Vehicles_Extension_02/Truck.java
@@ -1,11 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Vehicles_Extension_02;
+package Polymorphism_Exercises.Vehicles_Extension_02;
 
 public class Truck extends Vehicle {
-
     private static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 1.6;
 
     public Truck(double fuel, double consumption, double tankCapacity) {
-
         super(fuel, consumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION, tankCapacity);
     }
 
