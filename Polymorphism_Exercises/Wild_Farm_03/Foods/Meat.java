87ae3fae4af7a290f445b7015bb52e7684cb4902
Denis Buserski <denis.buserski@gmail.com>
2022-07-03 23:40:13 +0300

    Update Meat.java

--- Polymorphism_Exercises/Wild_Farm_03/Foods/Meat.java
+++ Polymorphism_Exercises/Wild_Farm_03/Foods/Meat.java
@@ -1,7 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Wild_Farm_03.Foods;
+package Polymorphism_Exercises.Wild_Farm_03.Foods;
 
 public class Meat extends Food {
     public Meat(int quantity) {
         super(quantity);
     }
+    
 }
