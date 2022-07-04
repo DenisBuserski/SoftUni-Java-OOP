38243570d393f19e6ec176ecf40211c246a97c81
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:45:05 +0300

    Update Mammal.java

--- Polymorphism_Exercises/Wild_Farm_03/Animals/Mammal.java
+++ Polymorphism_Exercises/Wild_Farm_03/Animals/Mammal.java
@@ -1,10 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Wild_Farm_03.Animals;
+package Polymorphism_Exercises.Wild_Farm_03.Animals;
 
-public abstract class Mammal extends Animal{
+public abstract class Mammal extends Animal {
 
     protected Mammal(String animalName, String type, double weight, String livingRegion) {
         super(animalName, type, weight, livingRegion);
     }
-
 
 }
