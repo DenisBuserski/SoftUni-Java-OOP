06f3ad50b563fc4c3730dd5a7942b146cfb84b2d
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:44:37 +0300

    Update Felime.java

--- Polymorphism_Exercises/Wild_Farm_03/Animals/Felime.java
+++ Polymorphism_Exercises/Wild_Farm_03/Animals/Felime.java
@@ -1,10 +1,9 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Wild_Farm_03.Animals;
+package Polymorphism_Exercises.Wild_Farm_03.Animals;
 
 public abstract class Felime extends Mammal {
 
     protected Felime(String animalName, String type, double weight, String livingRegion) {
         super(animalName, type, weight, livingRegion);
     }
-
 
 }
