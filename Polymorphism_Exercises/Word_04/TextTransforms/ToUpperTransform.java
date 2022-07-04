fa62d18e286789b64cc48149291a8d444e3597c7
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:50:09 +0300

    Update ToUpperTransform.java

--- Polymorphism_Exercises/Word_04/TextTransforms/ToUpperTransform.java
+++ Polymorphism_Exercises/Word_04/TextTransforms/ToUpperTransform.java
@@ -1,10 +1,12 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Word_04.TextTransforms;
+package Polymorphism_Exercises.Word_04.TextTransforms;
 
 public class ToUpperTransform implements TextTransform {
+    
     @Override
     public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
         for (int i = startIndex; i < endIndex; i++) {
             text.setCharAt(i, Character.toUpperCase(text.charAt(i)));
         }
     }
+    
 }
