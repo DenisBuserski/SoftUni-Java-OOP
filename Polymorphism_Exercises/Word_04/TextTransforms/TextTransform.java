5e527bc96a796f21adcd2a619fcd4bc739d1bc8b
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:49:38 +0300

    Update TextTransform.java

--- Polymorphism_Exercises/Word_04/TextTransforms/TextTransform.java
+++ Polymorphism_Exercises/Word_04/TextTransforms/TextTransform.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Word_04.TextTransforms;
+package Polymorphism_Exercises.Word_04.TextTransforms;
 
 public interface TextTransform {
     void invokeOn(StringBuilder text, int startIndex, int endIndex);
