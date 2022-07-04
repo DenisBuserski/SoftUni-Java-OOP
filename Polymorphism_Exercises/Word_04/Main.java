b8a449897220ce6cdab19e9935c4f678843129fb
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:54:09 +0300

    Update Main.java

--- Polymorphism_Exercises/Word_04/Main.java
+++ Polymorphism_Exercises/Word_04/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Word_04;
+package Polymorphism_Exercises.Word_04;
 
 import java.util.Scanner;
 
