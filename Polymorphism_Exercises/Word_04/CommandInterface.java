5f6cdf44f44972c53739a9eada4a6c65527ab9f0
Denis Buserski <denis.buserski@gmail.com>
2022-07-04 22:52:34 +0300

    Update CommandInterface.java

--- Polymorphism_Exercises/Word_04/CommandInterface.java
+++ Polymorphism_Exercises/Word_04/CommandInterface.java
@@ -1,6 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Polymorphism_Exercises.Word_04;
+package Polymorphism_Exercises.Word_04;
 
 public interface CommandInterface {
     void init();
+    
     void handleInput(String input);
 }
