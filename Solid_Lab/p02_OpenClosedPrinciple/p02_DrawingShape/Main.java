cc0c76e6a991ca5d2bd165f22a75995dd12a2bd2
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:21:46 +0300

    Update Main.java

--- Solid_Lab/p02_OpenClosedPrinciple/p02_DrawingShape/Main.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p02_DrawingShape/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p02_DrawingShape;
+package Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape;
 
 public class Main {
     public static void main(String[] args) {
