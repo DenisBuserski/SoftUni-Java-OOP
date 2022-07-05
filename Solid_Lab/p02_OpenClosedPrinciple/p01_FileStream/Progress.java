05f534236d7fa10a49e84f9e5a9bdb17f08daca7
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:20:27 +0300

    Update Progress.java

--- Solid_Lab/p02_OpenClosedPrinciple/p01_FileStream/Progress.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p01_FileStream/Progress.java
@@ -1,15 +1,14 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p01_FileStream;
+package Solid_Lab.p02_OpenClosedPrinciple.p01_FileStream;
 
 public class Progress {
     private final File file;
 
-    public Progress(File file)
-    {
+    public Progress(File file) {
         this.file = file;
     }
 
-    public int getCurrentPercent()
-    {
+    public int getCurrentPercent() {
         return this.file.getSent() * 100 / this.file.getLength();
     }
+    
 }
