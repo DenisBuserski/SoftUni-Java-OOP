b63c35cd71fced0dea83b96cdf4e4eb9c952f91b
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:11:00 +0300

    Update DrawingManagerImpl.java

--- Solid_Lab/p01_SingleResponsibility/p01_DrawingShape/DrawingManagerImpl.java
+++ Solid_Lab/p01_SingleResponsibility/p01_DrawingShape/DrawingManagerImpl.java
@@ -1,13 +1,11 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape;
+package Solid_Lab.p01_SingleResponsibility.p01_DrawingShape;
 
-
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingManager;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingRepository;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Rengerer;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Shape;
+import Solid_Lab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingManager;
+import Solid_Lab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingRepository;
+import Solid_Lab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Rengerer;
+import Solid_Lab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Shape;
 
 public class DrawingManagerImpl implements DrawingManager {
-
     private final DrawingRepository drawingRepository;
     private final Rengerer renderer;
 
@@ -16,9 +14,9 @@ public class DrawingManagerImpl implements DrawingManager {
         this.renderer = renderer;
     }
 
-
     @Override
     public void draw(Shape shape) {
         shape.draw(this.renderer, this.drawingRepository);
     }
+    
 }
