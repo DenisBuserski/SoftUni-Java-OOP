1be69b23f10fd08ce88428674133ccc3ba70af9a
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:23:03 +0300

    Update DrawingManagerImpl.java

--- Solid_Lab/p02_OpenClosedPrinciple/p02_DrawingShape/DrawingManagerImpl.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p02_DrawingShape/DrawingManagerImpl.java
@@ -1,30 +1,25 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p02_DrawingShape;
+package Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape;
 
-
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.DrawingManager;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
+import Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.DrawingManager;
+import Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
 
 public class DrawingManagerImpl implements DrawingManager {
 
-
     @Override
     public void draw(Shape shape) {
-        if (shape instanceof Circle)
-        {
+        if (shape instanceof Circle) {
             this.drawCircle((Circle)shape);
-        }
-            else if (shape instanceof Rectangle)
-        {
+        } else if (shape instanceof Rectangle) {
             this.drawRectangle(((Rectangle)shape));
         }
     }
-    public void drawCircle(Circle circle)
-    {
+    
+    public void drawCircle(Circle circle) {
         // Draw Circle
     }
 
-    public void drawRectangle(Rectangle rectangle)
-    {
+    public void drawRectangle(Rectangle rectangle) {
         // Draw Rectangle
     }
+    
 }
