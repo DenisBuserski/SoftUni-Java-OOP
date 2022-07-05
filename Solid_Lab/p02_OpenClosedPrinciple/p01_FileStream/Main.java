5cbe79f2e0ee65c90ef3345a1046d45832de4764
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:19:05 +0300

    Update Main.java

--- Solid_Lab/p02_OpenClosedPrinciple/p01_FileStream/Main.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p01_FileStream/Main.java
@@ -1,8 +1,7 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p01_FileStream;
+package Solid_Lab.p02_OpenClosedPrinciple.p01_FileStream;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
-
