a14bad4125cfbdee5ea64a2f8c0205f5ea7db2d1
Denis Buserski <denis.buserski@gmail.com>
2022-07-05 21:13:18 +0300

    Update DrawingRepository.java

--- Solid_Lab/p01_SingleResponsibility/p01_DrawingShape/interfaces/DrawingRepository.java
+++ Solid_Lab/p01_SingleResponsibility/p01_DrawingShape/interfaces/DrawingRepository.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces;
+package Solid_Lab.p01_SingleResponsibility.p01_DrawingShape.interfaces;
 
 public interface DrawingRepository {
 }
