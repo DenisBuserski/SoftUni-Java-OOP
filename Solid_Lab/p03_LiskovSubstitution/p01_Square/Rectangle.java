a150a999338db178a28b3bc60af4fb6766e6db65
Denis Buserski <denis.buserski@gmail.com>
2022-07-06 20:32:29 +0300

    Update Rectangle.java

--- Solid_Lab/p03_LiskovSubstitution/p01_Square/Rectangle.java
+++ Solid_Lab/p03_LiskovSubstitution/p01_Square/Rectangle.java
@@ -1,28 +1,28 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p03_LiskovSubstitution.p01_Square;
+package Solid_Lab.p03_LiskovSubstitution.p01_Square;
 
 public class Rectangle extends Shape {
    private double width;
-
    private double height;
-
-    public double getWidth() {
-        return this.width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-
-    public double getHeight() {
-        return this.height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    @Override
-    public double getArea() {
-       return this.width*this.height;
-    }
+   
+   public double getWidth() {
+      return this.width;
+   }
+   
+   public void setWidth(double width) {
+      this.width = width;
+   }
+   
+   public double getHeight() {
+      return this.height;
+   }
+   
+   public void setHeight(double height) {
+      this.height = height;
+   }
+   
+   @Override
+   public double getArea() {
+      return this.width*this.height;
+   }
+   
 }
