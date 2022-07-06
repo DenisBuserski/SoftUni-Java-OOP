c3f9a050f7c8aa11566d9c6b8959d0cd01f65427
Denis Buserski <denis.buserski@gmail.com>
2022-07-06 20:24:26 +0300

    Update Main.java

--- Solid_Lab/p02_OpenClosedPrinciple/p03_ShoppingCart/Main.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p03_ShoppingCart/Main.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p03_ShoppingCart;
+package Solid_Lab.p02_OpenClosedPrinciple.p03_ShoppingCart;
 
 public class Main {
     public static void main(String[] args) {
