d6f1dda299c2a980fdbc84467b261c8f7675cf23
Denis Buserski <denis.buserski@gmail.com>
2022-07-06 20:42:49 +0300

    Update Account.java

--- Solid_Lab/p04_InterfaceSegregation/p02_identity/interfaces/Account.java
+++ Solid_Lab/p04_InterfaceSegregation/p02_identity/interfaces/Account.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p04_InterfaceSegregation.p02_identity.interfaces;
+package Solid_Lab.p04_InterfaceSegregation.p02_identity.interfaces;
 
 public interface Account {
 
