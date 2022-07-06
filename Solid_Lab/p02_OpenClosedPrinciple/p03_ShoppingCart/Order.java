56e57ce29fe8bd869c4c1b22b01d699d167e5ac6
Denis Buserski <denis.buserski@gmail.com>
2022-07-06 20:27:46 +0300

    Update Order.java

--- Solid_Lab/p02_OpenClosedPrinciple/p03_ShoppingCart/Order.java
+++ Solid_Lab/p02_OpenClosedPrinciple/p03_ShoppingCart/Order.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p02_OpenClosedPrinciple.p03_ShoppingCart;
+package Solid_Lab.p02_OpenClosedPrinciple.p03_ShoppingCart;
 
 public abstract class Order {
     protected final Cart cart;
@@ -6,7 +6,9 @@ public abstract class Order {
     protected Order(Cart cart) {
         this.cart = cart;
     }
+    
     void chekout(){
         //log the order in the p03_Database
     }
+    
 }
