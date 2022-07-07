cc3e2c58873f5212991212f7b284dd8bb770d345
Denis Buserski <denis.buserski@gmail.com>
2022-07-07 23:07:34 +0300

    Update Appender.java

--- Solid_Exercise/src/interfaces/Appender.java
+++ Solid_Exercise/src/interfaces/Appender.java
@@ -1,6 +1,6 @@
 package interfaces;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.Logger_SOLID.enums.ReportLevel;
+import enums.ReportLevel;
 
 public interface Appender {
 
