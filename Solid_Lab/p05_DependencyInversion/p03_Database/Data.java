f1e5258d920aa97c866b3287dbbd9382bbc8981f
Denis Buserski <denis.buserski@gmail.com>
2022-07-07 22:01:50 +0300

    Update Data.java

--- Solid_Lab/p05_DependencyInversion/p03_Database/Data.java
+++ Solid_Lab/p05_DependencyInversion/p03_Database/Data.java
@@ -1,27 +1,25 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Solid.solidLab.p05_DependencyInversion.p03_Database;
+package Solid_Lab.p05_DependencyInversion.p03_Database;
 
 public class Data {
-    public Iterable<Integer> courseIds()
-    {
+    
+    public Iterable<Integer> courseIds() {
         // return course ids
         return null;
     }
 
-    public Iterable<String> courseNames()
-    {
+    public Iterable<String> courseNames() {
         // return course names
         return null;
     }
 
-    public Iterable<String> search(String substring)
-    {
+    public Iterable<String> search(String substring) {
         // return found results
         return null;
     }
 
-    public String getCourseById(int id)
-    {
+    public String getCourseById(int id) {
         // return course by id
         return null;
     }
+    
 }
