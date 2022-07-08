e8424a6525437d52330cb7c95d086154819e371c
Denis Buserski <denis.buserski@gmail.com>
2022-07-08 20:52:46 +0300

    Update Command.java

--- Design_Patterns_Lab/Command/Command.java
+++ Design_Patterns_Lab/Command/Command.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Lab.Command;
+package Design_Patterns_Lab.Command;
 
 public interface Command {
     String executeAction();
