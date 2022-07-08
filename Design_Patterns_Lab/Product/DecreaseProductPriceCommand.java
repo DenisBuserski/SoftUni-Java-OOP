bfce9b47a89e704f69b146cfb65a2a45775bf946
Denis Buserski <denis.buserski@gmail.com>
2022-07-08 20:57:24 +0300

    Update DecreaseProductPriceCommand.java

--- Design_Patterns_Lab/Product/DecreaseProductPriceCommand.java
+++ Design_Patterns_Lab/Product/DecreaseProductPriceCommand.java
@@ -1,4 +1,4 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Lab.Product;
+package Design_Patterns_Lab.Product;
 
 public class DecreaseProductPriceCommand implements Command{
     private final Product product;
