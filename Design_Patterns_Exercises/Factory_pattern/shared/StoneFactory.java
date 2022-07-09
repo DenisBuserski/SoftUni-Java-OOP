cae4e595c04467e0518023b9857e395df16a59ef
Denis Buserski <denis.buserski@gmail.com>
2022-07-09 23:45:50 +0300

    Update StoneFactory.java

--- Design_Patterns_Exercises/Factory_pattern/shared/StoneFactory.java
+++ Design_Patterns_Exercises/Factory_pattern/shared/StoneFactory.java
@@ -1,11 +1,13 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.shared;
+package Factory_pattern.shared;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.GameObject;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.Stone;
+import Factory_pattern.model.GameObject;
+import Factory_pattern.model.Stone;
 
 public class StoneFactory implements Factory {
+    
     @Override
     public GameObject produce() {
         return new Stone();
     }
+    
 }
