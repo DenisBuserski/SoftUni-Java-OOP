f3bca659e9fe9ea550b6773f5df926ad439b95bb
Denis Buserski <denis.buserski@gmail.com>
2022-07-09 23:44:12 +0300

    Update PlayerFactory.java

--- Design_Patterns_Exercises/Factory_pattern/shared/PlayerFactory.java
+++ Design_Patterns_Exercises/Factory_pattern/shared/PlayerFactory.java
@@ -1,16 +1,14 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.shared;
+package Factory_pattern.shared;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.GameObject;
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.Initializer;
+import Factory_pattern.model.GameObject;
+import Factory_pattern.model.Initializer;
 
 public class PlayerFactory implements Factory {
-
 
     @Override
     public GameObject produce() {
         int[] params = Initializer.dataStorage.load(Player.class);
-
         return new Player(params[0], params[1]);
-
     }
+    
 }
