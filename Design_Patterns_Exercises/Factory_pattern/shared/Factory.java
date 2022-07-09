d0331be63e41bee45e5c29bf4a9a78870dfb7db3
Denis Buserski <denis.buserski@gmail.com>
2022-07-09 23:41:08 +0300

    Update Factory.java

--- Design_Patterns_Exercises/Factory_pattern/shared/Factory.java
+++ Design_Patterns_Exercises/Factory_pattern/shared/Factory.java
@@ -1,6 +1,6 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.shared;
+package Factory_pattern.shared;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.GameObject;
+import Factory_pattern.model.GameObject;
 
 public interface Factory {
 
