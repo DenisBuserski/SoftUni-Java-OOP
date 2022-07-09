cb0796d98c35ebc0c7a5071a9610bf994c4002e0
Denis Buserski <denis.buserski@gmail.com>
2022-07-09 23:41:49 +0300

    Update InitialGameObjectFactory.java

--- Design_Patterns_Exercises/Factory_pattern/shared/InitialGameObjectFactory.java
+++ Design_Patterns_Exercises/Factory_pattern/shared/InitialGameObjectFactory.java
@@ -1,12 +1,11 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.shared;
+package Factory_pattern.shared;
 
-import JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model.GameObject;
+import Factory_pattern.model.GameObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InitialGameObjectFactory implements ProduceMultiple {
-
     private List<Factory> objectFactories;
 
     public InitialGameObjectFactory() {
@@ -26,6 +25,6 @@ public class InitialGameObjectFactory implements ProduceMultiple {
     @Override
     public void setFactories(List<Factory> objectFactories) {
         this.objectFactories = objectFactories;
-
     }
+    
 }
