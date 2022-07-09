b7ac5f3bfa29ec9dffa36dcd4ba2ac0df42f9940
Denis Buserski <denis.buserski@gmail.com>
2022-07-09 23:37:14 +0300

    Update GameObject.java

--- Design_Patterns_Exercises/Factory_pattern/model/GameObject.java
+++ Design_Patterns_Exercises/Factory_pattern/model/GameObject.java
@@ -1,10 +1,8 @@
-package JAVA_DEVELOPMENT_SOFTUNI.Java_Development.Java_Advanced.Java_OOP.Java_OOP_Exercises.Design_Patterns_Exercises.Factory_pattern.model;
+package Factory_pattern.model;
 
 public interface GameObject {
 
     void update();
 
     void render();
-
-
 }
