465c273d0368abe8246cef82d5641cd3ce61f93e
Denis Buserski <denis.buserski@gmail.com>
2022-07-31 13:27:23 +0300

    Add files via upload

---
+++ Unit_Testing_Lab/src/main/java/ex/Target.java
@@ -0,0 +1,13 @@
+package ex;
+
+public interface Target {
+    int getHealth();
+
+    void takeAttack(int attackPoints);
+
+    int giveExperience();
+
+    boolean isDead();
+
+    Weapon getLoot();
+}
